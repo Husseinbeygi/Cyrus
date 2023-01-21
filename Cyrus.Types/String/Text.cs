e8e748a710cda615964cb2a53cbbbfce8a19d51b
Hussein Beygi <hussein1997.hb@hotmail.com>
2023-01-21 21:07:03 +0330

    Change The Cyrus.Strings To Cyrus.Type And Added The Guid Sequential Generator

---
+++ Cyrus.Types/String/Text.cs
@@ -0,0 +1,35 @@
+﻿namespace Cyrus.Types.String
+{
+    public class Text
+    {
+
+        /// <summary>
+        ///  Trim spaces in the middle of Text 
+        /// </summary>
+        /// <param name="text">Text</param>
+        /// <returns></returns>
+        public static string TrimExtra(string text)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            text = text.Trim();
+            if (text == string.Empty)
+            {
+                return null;
+            }
+
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+
+
+
+            return text;
+        }
+
+    }
+}
